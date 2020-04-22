717efeee2e3e37f56e379ed4794d9f5677ee933a
64Harshit98 <43778378+64Harshit98@users.noreply.github.com>
2020-04-22 17:40:58 +0530

    final
    taken 1000 images to train

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://drive.google.com/uc?export=download&id=1PXWrVhTJYeGbBmE2DJ5_LEcstWSLqEKW'
+export_file_url = ''
 export_file_name = 'export.pkl'
 
 classes =['quote','document','photograph']
